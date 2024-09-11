4f99ba3e799077d1121945d1763b77900eb887fb
Abdel780 <abdelkarim27200@gmail.com>
2024-09-11 15:32:25 +0200

    mise a jour footer et header

--- partials/head.php
+++ partials/head.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet groupe Chat</title>
+    <title>Kitty Joy : Animalerie en ligne pour chats</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 </head>
+
+
 <body>
-    
-</body>
-</html>
+    <header>
+        <nav>
+            <div class="flexLogo">
+                <img src="/public/img/Cat_logo-removebg-preview.png" alt="">
+                <h1>Le coin des chats</h1>
+            </div>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Nourriture</a></li>
+                <li><a href="">Jouets</a></li>
+                <li><a href="">Accessoires</a></li>
+                <li><a href="">Qui sommes-nous</a></li>
+            </ul>
+        </nav>
+    </header>
